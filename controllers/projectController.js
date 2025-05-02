49945e608fae86f29f542892d9436f61e6b2e740
SriHarish555 <sriharish7635@gmail.com>
2025-05-03 01:49:50 +0530

    configs

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,33 +1,44 @@
-
 const road = require("../models/Road");
 const Config = require("../models/Config");
 
+const saveData = async (req, res) => {
+  try {
+    // Fetch the configuration to check if data should be saved
+    const config = await Config.findOne();
 
-const saveData = async (req,res)=>{
-   
-    try {
-        const config = await Config.findOne(); 
-            
-        if(config?.addData || false){
-        
-            const {latitude,longitude,distanceLeft,distanceRight} = req.body;
-            const roadData = new road({
-                ip:req.ip,
-                latitude,
-                longitude,
-                distanceLeft,
-                distanceRight,
-            });
-            roadData.save();
-            console.log("data saved in db ");
-            res.status(200).json({status:"success",msg:"data saved successfully"});
-        }
-        else{
-            console.log("data not saved in db ");
-            res.status(200).json({status:"failed",msg:"data not saved successfully"});
-        }
-    } catch (err) {
-        console.log("err", err);
+    // Check if data saving is allowed in the config
+    if (config?.addData || false) {
+      const { latitude, longitude, distanceLeft, distanceRight } = req.body;
+
+      // Create a new road data object
+      const roadData = new road({
+        ip: req.ip,
+        latitude,
+        longitude,
+        distanceLeft,
+        distanceRight,
+        location: {
+          type: "Point",
+          coordinates: [longitude, latitude], // [longitude, latitude] for GeoJSON format
+        },
+      });
+
+      // Save the road data to the database
+      await roadData.save();
+
+      // Log and respond to indicate success
+      console.log("Data saved in DB");
+      res.status(200).json({ status: "success", msg: "Data saved successfully" });
+    } else {
+      // If data saving is not allowed, log and respond with failure
+      console.log("Data not saved in DB");
+      res.status(200).json({ status: "failed", msg: "Data not saved" });
     }
-}
+  } catch (err) {
+    // Log and respond with an error if something goes wrong
+    console.error("Error saving data:", err);
+    res.status(500).json({ status: "failed", msg: "Internal Server Error", error: err.message });
+  }
+};
+
 module.exports = { saveData };
--- models/Road.js
+++ models/Road.js
@@ -2,34 +2,47 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const roadSchema = new mongoose.Schema(
-    {
-        ip: {
-            type: String,
-            required: true,
-        },
-        latitude: {
-            type: Number,
-            required: true,
-        },
-        longitude: {
-            type: Number,
-            required: true,
-        },
-        distanceLeft: {
-            type: Number,
-            required: true,
-        },
-        distanceRight: {
-            type: Number,
-            required: true,
-        },
-        timestamp: {
-            type: Date,
-            default: Date.now,
-        }
+  {
+    ip: {
+      type: String,
+      required: true,
     },
-    { timestamps: true }
+    latitude: {
+      type: Number,
+      required: true,
+    },
+    longitude: {
+      type: Number,
+      required: true,
+    },
+    distanceLeft: {
+      type: Number,
+      required: true,
+    },
+    distanceRight: {
+      type: Number,
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+    location: {
+      type: {
+        type: String, 
+        enum: ['Point'], 
+        required: true
+      },
+      coordinates: {
+        type: [Number], 
+        required: true
+      }
+    }
+  },
+  { timestamps: true }
 );
+
+roadSchema.index({ location: "2dsphere" });
 
 const Road = mongoose.model("Road", roadSchema);
 
