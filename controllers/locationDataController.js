567906a4cd59ac339280a9742de93913e6cc5f68
SriHarish555 <sriharish7635@gmail.com>
2025-05-05 00:21:30 +0530

    location added

---
+++ controllers/locationDataController.js
@@ -0,0 +1,32 @@
+const road = require("../models/Road");
+
+function generateHazardPoints(data) {
+  return data
+    .map(doc => {
+      const { latitude, longitude, distanceLeft, distanceRight } = doc;
+
+      if (distanceLeft < 25 || distanceRight < 25) {
+        return { latitude, longitude, type: "bump" };
+      }
+
+      if (distanceLeft > 40 || distanceRight > 40) {
+        return { latitude, longitude, type: "pothole" };
+      }
+
+      return null; 
+    })
+    .filter(Boolean); 
+}
+
+const getLocation = async (req, res) => {
+  try {
+    const data = await road.find(); 
+    const hazards = generateHazardPoints(data); 
+    res.status(200).json(hazards); 
+  } catch (error) {
+    console.error("Error fetching road data:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+module.exports = { getLocation };
--- routes/project.js
+++ routes/project.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { saveData } = require("../controllers/projectController");
-const {analyze} = require("../controllers/roadAnalysisController")
+const {analyze} = require("../controllers/roadAnalysisController");
+const {getLocation} = require ("../controllers/locationController");
 
 
 router.post("/data",saveData);
-router.get("/analyze",analyze)
+router.get("/analyze",analyze);
+router.get("/get-loc",getLocation);
 
 module.exports = router;
